e38926b3f6dae3f69baae7b31f22c844e24311eb
Ken Eucker <keneucker@gmail.com>
2017-02-05 01:00:35 -0800

    Made the profile image a scroll to the top button.

--- templates/ken/js/index.js
+++ templates/ken/js/index.js
@@ -46,3 +46,20 @@ window.onload = function() {
         }
     }
 };
+
+function scrollTo(element, to, duration) {
+  if (duration <= 0) return;
+  var difference = to - element.scrollTop;
+  var perTick = difference / duration * 10;
+
+  setTimeout(function() {
+    element.scrollTop = element.scrollTop + perTick;
+    if (element.scrollTop == to) return;
+    scrollTo(element, to, duration - 10);
+  }, 10);
+}
+
+document.querySelector('#badge').addEventListener('click', function() {
+    //scroll to top
+    scrollTo(document.body, 0, 600);
+});
